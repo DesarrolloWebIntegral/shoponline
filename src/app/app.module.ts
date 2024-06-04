df2eafb4a286084b8ef5d379bd0710ce5fde5725
gbarronutng <barg90@hotmail.com>
2024-06-04 14:38:28 -0600

    Versión Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { PrimengModule } from './primeng.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
